37fdd301caca230b26c0390979fd87290f015ee3
himaloyash <himaloyashis98@gmail.com>
2023-11-01 18:37:39 +0600

    nav bar responsive almost done

--- src/Page/Shared/Navbar/Navbar.jsx
+++ src/Page/Shared/Navbar/Navbar.jsx
@@ -7,21 +7,24 @@ const Navbar = () => {
     const [Open, setOpen] = useState(false)
 
     return (
-        <div className=''>
-            <div className='bg-[#164863] py-2 px-2'>
-                <span className=''>
-                    <XMarkIcon className="h-6 w-6 text-blue-500" />
-                    <Bars3Icon className="h-6 w-6 text-blue-500" />
+        <div className='bg-[#164863] py-2 px-2 '>
+            <div className=''>
+                <span onClick={() => setOpen(!Open)} className='md:hidden'>
+
+                    {
+                        Open === true ? < XMarkIcon className="h-6 w-6 text-blue-500 cursor-wait" />
+                            : <Bars3Icon className="h-6 w-6 text-blue-500 cursor-pointer" />
+                    }
                 </span>
             </div>
-            <ul>
+            <ul className={` md:flex bg-[#164863] py-3 px-2 rounded-md mt-3 duration-500 text-white font-bold absolute  ${Open? 'top-6' :  "-top-56"} `}>
 
-                <li><Link>DevTown</Link></li>
-                <li><Link>DevTown</Link></li>
-                <li><Link>DevTown</Link></li>
-                <li><Link>DevTown</Link></li>
-                <li><Link>DevTown</Link></li>
-                <li><Link>DevTown</Link></li>
+                <li className='mb-2'><Link>DevTown</Link></li>
+                <li className='mb-2'><Link>DevTown</Link></li>
+                <li className='mb-2'><Link>DevTown</Link></li>
+                <li className='mb-2'><Link>DevTown</Link></li>
+                <li className='mb-2'><Link>DevTown</Link></li>
+                <li className='mb-2'><Link>DevTown</Link></li>
             </ul>
 
         </div>
