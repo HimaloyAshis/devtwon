5a5d5d685999373fc8b5eb5ba7aabcda8fa7db4a
himaloyash <himaloyashis98@gmail.com>
2023-11-01 12:54:33 +0600

    navbar decorated

--- src/Page/Shared/Navbar/Navbar.jsx
+++ src/Page/Shared/Navbar/Navbar.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div>
+        <div className=''>
+            <div className='bg-[#164863] py-2 px-2'>
+                <p className='text-white'>DevTown</p>
+            </div>
+            <ul>
+
+                <li><Link>DevTown</Link></li>
+                <li><Link>DevTown</Link></li>
+                <li><Link>DevTown</Link></li>
+                <li><Link>DevTown</Link></li>
+                <li><Link>DevTown</Link></li>
+                <li><Link>DevTown</Link></li>
+            </ul>
             
         </div>
     );
